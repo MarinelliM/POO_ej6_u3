6bda53ab015d1d751f8b7ad65cd8e8acbf278c62
MarinelliM <103908022+MarinelliM@users.noreply.github.com>
2022-06-02 22:34:53 -0300

    Create Interfaz.py

---
+++ Interfaz.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+
+class Intefaz(Interface):
+
+    def insertarElemento(self, elemento, pos):
+        pass
+
+    def agregarElemento(self, elemento):
+        pass
+
+    def mostrarElemento(self):
+        pass
